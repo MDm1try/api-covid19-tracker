7222d6aa524eecef38d1f2ffce7b0fbd64f2a326
MDm1try <dima.miroshni4enko@gmail.com>
2021-03-14 23:40:33 +0200

    Added seraching by customers

--- routes/secure/customers/get.js
+++ routes/secure/customers/get.js
@@ -2,16 +2,25 @@ const Users = require('../../../models/Users')
 const { USER_TYPES } = require('../../../utils/constants')
 
 const create = async (req, res) => {
-
-    // const projection = {
-    //     _id: 1,
-    //     firstName: 1,
-    //     lastName: 1,
-    //     email: 1,
-    //     dob: 1,
-    //     statuses: 1
-    // }
-    const users = await Users.find({ type: USER_TYPES.CUSTOMER }).populate('statuses')
+    const name = req.query.name || ''
+    const projection = {
+        _id: 1,
+        firstName: 1,
+        lastName: 1,
+        email: 1,
+        dob: 1,
+        statuses: 1
+    }
+    const users = await Users.find({
+        '$expr': {
+            '$regexMatch': {
+                'input': { '$concat': ['$firstName', ' ', '$lastName'] },
+                'regex': name,  //Your text search here
+                'options': 'i'
+            }
+        },        
+        type: USER_TYPES.CUSTOMER
+    }, projection).populate('statuses')
 
     return res.status(200).send({ users }) 
 }
