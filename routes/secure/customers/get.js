63aa61ff555efc06d9d84c534d4d4e09fa62b695
MDm1try <dima.miroshni4enko@gmail.com>
2021-03-14 22:49:28 +0200

    Fix

--- routes/secure/customers/get.js
+++ routes/secure/customers/get.js
@@ -3,15 +3,15 @@ const { USER_TYPES } = require('../../../utils/constants')
 
 const create = async (req, res) => {
 
-    const projection = {
-        _id: 1,
-        firstName: 1,
-        lastName: 1,
-        email: 1,
-        dob: 1,
-        statuses: 1
-    }
-    const users = await Users.find({ type: USER_TYPES.CUSTOMER }, projection).populate('statuses')
+    // const projection = {
+    //     _id: 1,
+    //     firstName: 1,
+    //     lastName: 1,
+    //     email: 1,
+    //     dob: 1,
+    //     statuses: 1
+    // }
+    const users = await Users.find({ type: USER_TYPES.CUSTOMER }).populate('statuses')
 
     return res.status(200).send({ users }) 
 }
