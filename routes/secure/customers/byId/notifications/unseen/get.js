0401fb82cb7ab7e3d74b7907d6f02ddc3763ec3e
MDm1try <dima.miroshni4enko@gmail.com>
2021-03-31 18:36:41 +0300

    Fixes

--- routes/secure/customers/byId/notifications/unseen/get.js
+++ routes/secure/customers/byId/notifications/unseen/get.js
@@ -4,7 +4,7 @@ const get = async (req, res) => {
     const id = req.params.id
     try {
         const unseenNotifications = await Notifications.countDocuments({ toUser: id, seen: false })
-        return res.status(200).send(unseenNotifications) 
+        return res.status(200).send({ unseenNotifications }) 
     } catch(err) {
         console.error(err)
         return res.status(500).send({ error: err.message }) 
